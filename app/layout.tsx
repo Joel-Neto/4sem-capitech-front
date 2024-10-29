a27b02977af6cf3ed1c224266d12ca0a0ae47afe
Joel Neto <joelalvesneto187@gmail.com>
2024-10-28 22:54:35 -0300

    ✨ Feat: Initial system architecture
    Initial system architecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import Header from "@/components/UI/organisms/header";
+import Footer from "@/components/UI/organisms/footer";
 
 export const metadata: Metadata = {
   title: {
@@ -38,26 +40,16 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
+            <Header />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer>
+            <Footer /> 
           </div>
         </Providers>
       </body>
