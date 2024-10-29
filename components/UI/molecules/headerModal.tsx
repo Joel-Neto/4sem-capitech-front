fdeaeb18dd28f62537bb2b8d83ebab632277dd2a
Joel Neto <joelalvesneto187@gmail.com>
2024-10-28 22:55:29 -0300

    ✨ Feat: Creation of header and header navigation modal
    Creation of

---
+++ components/UI/molecules/headerModal.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@nextui-org/link";
+import React from "react";
+import { IoClose } from "react-icons/io5";
+
+import { siteConfig } from "@/config/site";
+
+interface IHeaderModal {
+  showAndCloseModal: () => void;
+}
+
+export default function HeaderModal({ showAndCloseModal }: IHeaderModal) {
+  return (
+    <div className="absolute z-['9999'] top-0 left-0 bg-black w-64  rounded-e-xl">
+      <div className="relative w-full h-full rounded-e-xl py-10 px-5">
+        <button className="absolute right-4 top-4" onClick={showAndCloseModal}>
+          <IoClose className="pointer-events-none" size={25} />
+        </button>
+
+        <ul className="flex flex-col gap-2 text-md">
+          {siteConfig.menuItems.map((link, i) => (
+            <li key={`link-${i}`}>
+              <Link color="foreground" aria-busy href={link.href}>
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
