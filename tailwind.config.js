4b5cb9976d16b0b2816d28f0865a9f710b90824e
Joel Neto <joelalvesneto187@gmail.com>
2024-10-28 22:53:32 -0300

    💄 Style: Website colors
    Website colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +13,35 @@ module.exports = {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
       },
+      colors: {
+        capi_purple: "#304076",
+        capi_yellow: "#EDB604",
+        capi_green: "#63C770",
+        capi_blue: "#1EA0D1",
+        capi_red: "#FB3535",
+        capi_blue_home: "#5DA6DB",
+        capi_blue_home_darker: "#5190bd",
+        capi_gray_home: "#e5e7eb",
+        capi_gray_home_darker: "#4b5563",
+        capi_gray_login: "#838183",
+        capi_gray_login_darker: "#1d1c1d",
+        capi_gray_contact: "#3E3E3E",
+        capi_seaGreen_about: "#04D9B3",
+        capi_seaGreen_about_light: "#0EFFE2",
+        capi_gray_development: "#2d2d2d",
+      },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            foreground: "#FFFFFF",
+          },
+        },
+      },
+    }),
+  ],
+};
