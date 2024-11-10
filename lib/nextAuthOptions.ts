1da139e8295af31a6de4877bdaef3b3865e11e5a
Joel Neto <joelalvesneto187@gmail.com>
2024-11-10 19:44:06 -0300

    ✨ Feat(next-auth): NextAuth config
    NextAuth config

---
+++ lib/nextAuthOptions.ts
@@ -0,0 +1,48 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import api from "@/services/axios";
+
+export const nextAuthOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials) return null;
+
+        console.log(credentials);
+
+        const response = await api.post("/login", credentials);
+
+        const userData = await response.data.data;
+
+        console.log(userData);
+
+        if (userData && response.status === 200) {
+          return userData;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      user && (token.user = user);
+
+      return token;
+    },
+    async session({ session, token }) {
+      session = token.user as any;
+
+      return session;
+    },
+  },
+};
