845e7c1c4f4a0ae502d68fd70778f3be5ab632a7
Joel Neto <joelalvesneto187@gmail.com>
2024-11-23 10:10:12 -0300

    ✨ Feat: Creation of trail typing
    Creation of trail typing

---
+++ types/Trail.ts
@@ -0,0 +1,9 @@
+export interface Trail {
+  _id: string;
+  description: string;
+  video_description: string;
+  name: string;
+  references: string;
+  subtitle: string;
+  video_title: string;
+}
