7d54b8fcb6025e81acc1fd083e7795a0c1e22179
Gabriel Fortes Ashikaga <gabriel.ashikaga@outlook.com>
2024-11-22 21:57:24 -0300

    ✨ Feat(footer): Adding link directing home to the logo
    Adding link directing home to the logo

--- components/UI/organisms/footer.tsx
+++ components/UI/organisms/footer.tsx
@@ -9,9 +9,12 @@ export default function Footer() {
     <footer className="bg-capi_purple p-5">
       <div className="container mx-auto max-w-5xl text-white mb-3">
         <div className="flex items-center justify-center ">
-          <h4 className="font-headline text-3xl font-bold">Capi</h4>
-          <Image height={65} src="/assets/logo.png" width={48} />
-          <h4 className="font-headline text-3xl font-bold">Tech</h4>
+          <Link href="/" className="text-white">
+            <h4 className="font-headline text-3xl font-bold">Capi</h4>
+            <Image height={65} src="/assets/logo.png" width={48} />
+            <h4 className="font-headline text-3xl font-bold">Tech</h4>
+          </Link>
+
         </div>
       </div>
       <div className="text-center">
