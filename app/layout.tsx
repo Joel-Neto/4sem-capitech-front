95ca4e9a4a19a4808a3c6580b1da70c60de68ebd
Gabriel Fortes Ashikaga <gabriel.ashikaga@outlook.com>
2024-12-07 22:33:40 -0300

    :bug: Fix(layout): update Hotjar script initialization to use lazy loading for improved performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,19 +37,15 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
+        <Script strategy="lazyOnload" />
+          {`(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:5234029,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
         />
       </head>
       <body
